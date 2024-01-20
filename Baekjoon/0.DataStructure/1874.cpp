fd2666120b07732f47b8c9a5eb078c3efe36a386
JengDeuk <ljbdev0927@gmail.com>
2024-01-20 16:33:41 +0900

    2024.01.20

---
+++ Baekjoon/0.DataStructure/1874.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	bool possible = true;
+
+	int n = 0;
+	cin >> n;
+
+	vector<int> sequence;
+	sequence.reserve(n);
+	for (int i = 0; i < n; ++i)
+	{
+		int inputNumber = 0;
+		cin >> inputNumber;
+		sequence.emplace_back(inputNumber);
+	}
+
+	stack<int> outputStack;
+	for (int i = n; i > 0; --i)
+	{
+		outputStack.push(i);
+	}
+
+	queue<bool> result;
+	stack<int> inputStack;
+	for (int i = 0; i < n; ++i)
+	{
+		while ((inputStack.empty() || inputStack.top() < sequence[i])
+			&& !outputStack.empty())
+		{
+			inputStack.push(outputStack.top());
+			outputStack.pop();
+			result.push(true);
+		}
+
+		if (inputStack.top() == sequence[i])
+		{
+			inputStack.pop();
+			result.push(false);
+			continue;
+		}
+		else
+		{
+			possible = false;
+			break;
+		}
+	}
+
+	if (!possible)
+	{
+		cout << "NO\n";
+		return 0;
+	}
+
+	while (!result.empty())
+	{
+		if (result.front())
+		{
+			cout << "+\n";
+		}
+		else
+		{
+			cout << "-\n";
+		}
+		result.pop();
+	}
+}
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 [![Solved.ac Profile](http://mazassumnida.wtf/api/v2/generate_badge?boj=jengdeuk)](https://solved.ac/jengdeuk/)
 
-- 사용 언어 : C++
-- 문제풀이 사이트 : [백준](https://www.acmicpc.net/) ( [프로필](https://www.acmicpc.net/user/jengdeuk) ) [프로그래머스](https://programmers.co.kr/)
-- [문제 풀이 올리는  곳](https://jengdeuk.tistory.com/)
-- [알고리즘 정리하고 올리는 곳](https://jengdeuk.tistory.com/)
+- 언어 : C++
+- 사이트 : [백준](https://www.acmicpc.net/)([프로필](https://www.acmicpc.net/user/jengdeuk)) [프로그래머스](https://programmers.co.kr/)
+- [문제 풀이](https://jengdeuk.tistory.com/)
+- [알고리즘 공부](https://jengdeuk.tistory.com/)
 
 ----
 - 2024.01.20 시작
