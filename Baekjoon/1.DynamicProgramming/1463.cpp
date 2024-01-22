f59adec23c3715f8ec2be9e90011f015fa2379f1
JengDeuk <ljbdev0927@gmail.com>
2024-01-22 13:50:48 +0900

    24.01.22

---
+++ Baekjoon/1.DynamicProgramming/1463.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+static int minOp[1000001];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	minOp[2] = minOp[3] = 1;
+	for (int i = 4; i <= n; ++i)
+	{
+		int op3 = 100, op2 = 100, op1 = 100;
+
+		if (i % 3 == 0)
+		{
+			op3 = min(minOp[i / 3], minOp[i - 1]) + 1;
+		}
+
+		if (i % 2 == 0)
+		{
+			op2 = min(minOp[i / 2], minOp[i - 1]) + 1;
+		}
+
+		op1 = minOp[i] = minOp[i - 1] + 1;
+
+		minOp[i] = min(min(op3, op2), op1);
+	}
+
+	cout << minOp[n];
+}
---
+++ Baekjoon/1.DynamicProgramming/2839.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int weight = 0;
+	int i = 0, j = 0, result = -1;
+	bool finished = false;
+	while (!finished)
+	{
+		while (true)
+		{
+			weight = 5 * j + 3 * i;
+			if (weight == n)
+			{
+				finished = true;
+				result = i + j;
+				break;
+			}
+			else if (weight > n)
+			{
+				j = 0;
+				break;
+			}
+
+			++j;
+		}
+
+		++i;
+		if (5 * j + 3 * i > n)
+		{
+			finished = true;
+		}
+	}
+
+	cout << result;
+}
