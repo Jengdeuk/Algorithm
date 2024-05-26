e2918c2192844ffc37708e66e8e6c39e7f55cf07
jengdeuk <jengdeuk@gmail.com>
2024-05-26 18:46:50 +0900

    24.05.26

---
+++ Baekjoon/97.Rank/0.Bronze/14581.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	string S;
+	cin >> S;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << S << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+}
