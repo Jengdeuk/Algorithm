cf10642bb75672cb7cc6e6cf467da78577ef006d
jengdeuk <jengdeuk@gmail.com>
2024-08-16 18:43:12 +0900

    24.08.16

---
+++ Baekjoon/97.Rank/0.Bronze/24723.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int N;
+	cin >> N;
+	cout << pow(2, N);
+}
