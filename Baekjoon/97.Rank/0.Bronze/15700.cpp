8cd515d41f11e70c2041a017f6eace638ba75ad4
jengdeuk <jengdeuk@gmail.com>
2024-12-10 09:49:34 +0900

    24.12.10

---
+++ Baekjoon/97.Rank/0.Bronze/15700.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	long long N, M;
+	cin >> N >> M;
+	cout << N * M / 2;
+}
