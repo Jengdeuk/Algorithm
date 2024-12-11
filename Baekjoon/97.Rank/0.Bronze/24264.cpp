ba3eac55d42c75ac095add6005228bd4ca905051
Jongbeom Lee <jengdeuk@gmail.com>
2024-12-11 12:42:59 +0900

    24.12.11

--- Baekjoon/97.Rank/0.Bronze/24264.cpp
+++ Baekjoon/97.Rank/0.Bronze/24264.cpp
@@ -6,8 +6,8 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
     
-  long long N;
-  cin >> N;
-  cout << N * N << '\n';
-  cout << 2;
+	long long N;
+	cin >> N;
+	cout << N * N << '\n';
+	cout << 2;
 }
