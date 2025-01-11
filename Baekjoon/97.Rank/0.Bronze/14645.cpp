974a16dd88822ff1bebf3d06a4435be55e02d1d1
jengdeuk <jengdeuk@gmail.com>
2025-01-11 15:50:45 +0900

    25.01.11

---
+++ Baekjoon/97.Rank/0.Bronze/14645.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int N, K;
+	cin >> N >> K;
+	for (int i = 0; i < N; ++i)
+	{
+		int A, B;
+		cin >> A >> B;
+	}
+
+	cout << "ºñ¿ÍÀÌ";
+}
