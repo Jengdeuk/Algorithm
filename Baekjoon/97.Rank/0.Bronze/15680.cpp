c59f28f84f2cf236a8aacf267767835b0f20ef0e
Jengdeuk <jengdeuk@gmail.com>
2025-01-17 22:50:15 +0900

    25.01.17

---
+++ Baekjoon/97.Rank/0.Bronze/15680.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	if (n == 0)
+	{
+		cout << "YONSEI";
+	}
+	else
+	{
+		cout << "Leading the Way to the Future";
+	}
+}
