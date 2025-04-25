ab083b0ccb71bfead06dacc73e185a9b061e114d
jengdeuk <jengdeuk@gmail.com>
2025-04-26 06:53:38 +0900

    25.04.26

---
+++ Baekjoon/97.Rank/0.Bronze/10833.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	int sum = 0;
+	while (n--)
+	{
+		int a, b;
+		cin >> a >> b;
+		sum += b % a;
+	}
+
+	cout << sum;
+}
