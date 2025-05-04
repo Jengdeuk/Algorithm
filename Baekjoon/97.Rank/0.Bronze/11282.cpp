613b36d29a9cdc88710d0b84089f3603b5ed19d1
jengdeuk <jengdeuk@gmail.com>
2025-05-04 23:22:20 +0900

    25.05.04

---
+++ Baekjoon/97.Rank/0.Bronze/11282.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	n += 3071;
+
+	string s;
+	s.push_back(n / 4096 + 234);
+	s.push_back(n / 64 % 64 + 128);
+	s.push_back(n % 64 + 128);
+
+	cout << s;
+}
