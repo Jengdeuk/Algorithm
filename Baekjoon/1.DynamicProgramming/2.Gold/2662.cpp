5e5cb7b2afb01e4d258d7e2182c01d78d2622410
jengdeuk <jengdeuk@gmail.com>
2025-08-12 23:45:06 +0900

    25.08.12

--- Baekjoon/1.DynamicProgramming/2.Gold/2662.cpp
+++ Baekjoon/1.DynamicProgramming/2.Gold/2662.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 using namespace std;
 
-typedef pair<int, int> p;
-
 int N, M, P[301][21], DP[301][21];
-p T[301][21];
 
 int main()
 {
@@ -22,21 +19,35 @@ int main()
 		}
 	}
 
-	for (int n = 1; n <= N; ++n)
+	for (int i = 1; i <= M; ++i)
 	{
-		for (int j = 1; j <= M; ++j)
+		for (int j = N; j >= 0; --j)
 		{
-			for (int i = 0; i <= n; ++i)
+			for (int k = 0; k <= j; ++k)
 			{
-				int NP = DP[n - i][j - 1] + P[i][j];
-				if (NP > DP[n][j])
+				int NDP = DP[j - k][0] + P[k][i];
+				if (NDP > DP[j][0])
 				{
-					DP[n][j] = NP;
-					T[n][j] = p(j, i);
+					DP[j][0] = NDP;
+					for (int l = 1; l <= M; ++l)
+					{
+						if (l == i)
+						{
+							DP[j][i] = k;
+						}
+						else
+						{
+							DP[j][l] = DP[j - k][l];
+						}
+					}
 				}
 			}
 		}
 	}
 
-	int a = 0;
+	cout << DP[N][0] << '\n';
+	for (int i = 1; i <= M; ++i)
+	{
+		cout << DP[N][i] << ' ';
+	}
 }
