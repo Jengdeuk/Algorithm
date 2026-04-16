5e33af26aa338eb1f4560dba7db17c991e595393
jengdeuk <jengdeuk@gmail.com>
2026-04-16 19:07:19 +0900

    26.04.16

--- Baekjoon/13.Kruskal/2.Gold/1647.cpp
+++ Baekjoon/13.Kruskal/2.Gold/1647.cpp
@@ -1,83 +1,81 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
 #include <algorithm>
 using namespace std;
 
-vector<tuple<int, int, int>> Edges;
-int RootNode[100000];
-
-int Find(int Node)
+struct Edge
 {
-	if (Node == RootNode[Node])
+	int Cost;
+	int A;
+	int B;
+
+	bool operator<(const Edge& Other) const
 	{
-		return Node;
+		return Cost < Other.Cost;
+	}
+};
+
+const int MaxNode = 100000;
+const int MaxEdge = 1000000;
+
+int N, M, R[MaxNode + 1];
+Edge E[MaxEdge];
+
+int Find(int A)
+{
+	if (R[A] == A)
+	{
+		return A;
 	}
 
-	return RootNode[Node] = Find(RootNode[Node]);
+	return R[A] = Find(R[A]);
 }
 
-void Union(int NodeA, int NodeB)
+void Union(int A, int B)
 {
-	int A = Find(NodeA);
-	int B = Find(NodeB);
-
-	int Min = min(A, B);
-	RootNode[A] = Min;
-	RootNode[B] = Min;
-}
-
-int Kruskal(int N, int M)
-{
-	if (N == 2)
-	{
-		return 0;
-	}
-
-	int Cost = 0;
-	int Linked = 0;
-	for (int i = 0; i < M; ++i)
-	{
-		int NodeA = get<1>(Edges[i]);
-		int NodeB = get<2>(Edges[i]);
-		if (Find(NodeA) != Find(NodeB))
-		{
-			Union(NodeA, NodeB);
-			Cost += get<0>(Edges[i]);
-			++Linked;
-
-			if (Linked == N - 2)
-			{
-				break;
-			}
-		}
-	}
-
-	return Cost;
+	R[Find(B)] = Find(A);
 }
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	cin.tie(nullptr); cout.tie(nullptr);
 
-	int N, M;
 	cin >> N >> M;
-
-	for (int i = 0; i < N; ++i)
+	for (int i = 1; i <= N; ++i)
 	{
-		RootNode[i] = i;
+		R[i] = i;
 	}
 
-	Edges.reserve(M);
 	for (int i = 0; i < M; ++i)
 	{
-		int A, B, C;
-		cin >> A >> B >> C;
-		Edges.emplace_back(C, A - 1, B - 1);
+		cin >> E[i].A >> E[i].B >> E[i].Cost;
 	}
 
-	sort(Edges.begin(), Edges.end());
+	sort(E, E + M);
 
-	cout << Kruskal(N, M);
+	int Count = 0;
+	int Sum = 0;
+	for (int i = 0; i < M; ++i)
+	{
+		if (Find(E[i].A) == Find(E[i].B))
+		{
+			continue;
+		}
+
+		Sum += E[i].Cost;
+		Union(E[i].A, E[i].B);
+		if (++Count == N - 2)
+		{
+			break;
+		}
+	}
+
+	if (N == 2)
+	{
+		cout << 0;
+	}
+	else
+	{
+		cout << Sum;
+	}
 }
